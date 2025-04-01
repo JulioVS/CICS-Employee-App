7cb1af4f8fe5046220b854c7beb6924435f911b0
JulioVS <julioer@me.com>
2025-04-01 18:21:36 -0300

    Refactor COBOL code

--- CBL/ESONP.cbl
+++ CBL/ESONP.cbl
@@ -24,69 +24,96 @@
        01 DFHCOMMAREA          PIC X(16).
 
        PROCEDURE DIVISION.
+       MAIN-LOGIC SECTION.
+      *
            IF EIBCALEN IS EQUAL TO ZERO
-      *       THIS IS THE START OF THE (PSEUDO) CONVERSATION,
-      *       MEANING THE FIRST INTERACTION OF THE PROCESS,
-      *       HENCE THE EMPTY COMM-AREA.-
-              INITIALIZE WS-SESSION-STATE
-              INITIALIZE ESONMO
-
-      *       FOR THE FIRST INTERACTION, IT SENDS THE EMPY MAP WITH
-      *       JUST THE TRANSACTION ID ON IT (AN ECHO OF A KNOWN VALUE)
-              MOVE EIBTRNID TO TRANIDO
-
-              EXEC CICS SEND
-                   MAP('ESONM')
-                   MAPSET('ESONMAP')
-                   FROM (ESONMO)
-                   ERASE
-                   END-EXEC
-
-      *       THEN, IT RETURNS JUST SAVING THE STILL-EMPTY STATE
-      *       AND ENDING THIS STEP OF THE CONVERSATION.
-              EXEC CICS RETURN
-                   COMMAREA(WS-SESSION-STATE)
-                   TRANSID(EIBTRNID)
-                   END-EXEC
+              PERFORM 1000-FIRST-INTERACTION
            ELSE
-      *       THIS IS THE CONTINUATION OF THE CONVERSATION,
-      *       MEANING THE SECOND INTERACTION OF THE PROCESS,
-      *       HENCE THE COMM-AREA IS NOT EMPTY.
-
-      *       RESTORE PREVIOUS SESSION DATA INTO MY VARS
-              MOVE DFHCOMMAREA TO WS-SESSION-STATE
-
-      *       GET INPUT FROM USER
-              EXEC CICS RECEIVE
-                   MAP('ESONM')
-                   MAPSET('ESONMAP')
-                   INTO (ESONMI)
-                   END-EXEC
-
-      *       IF NEW DATA WAS SENT, UPDATE STATE
-              IF USERIDI IS NOT EQUAL TO LOW-VALUES
-                 MOVE USERIDI TO WS-USER-ID
-              END-IF
-              IF PASSWDI IS NOT EQUAL TO LOW-VALUES
-                 MOVE PASSWDI TO WS-USER-PASSWORD
-              END-IF
-
-      *       SEND THE MAP BACK WITH A GREETING!
-              STRING "Hello " DELIMITED BY SIZE
-                     WS-USER-ID DELIMITED BY SPACE
-                     "!" DELIMITED BY SIZE
-                 INTO MESSO
-              END-STRING
-
-              EXEC CICS SEND
-                   MAP('ESONM')
-                   MAPSET('ESONMAP')
-                   FROM (ESONMO)
-                   ERASE
-                   END-EXEC
-
-              EXEC CICS RETURN
-                   COMMAREA(WS-SESSION-STATE)
-                   TRANSID(EIBTRNID)
-                   END-EXEC
+              PERFORM 2000-PROCESS-USER-INPUT
            END-IF.
+
+       SUB-ROUTINE SECTION.
+      *
+       1000-FIRST-INTERACTION.
+      *    THIS IS THE START OF THE (PSEUDO) CONVERSATION,
+      *    MEANING THE FIRST INTERACTION OF THE PROCESS,
+      *    HENCE THE EMPTY COMM-AREA
+           PERFORM 1100-INITIALIZE.
+           PERFORM 1200-SEND-MAP.
+           PERFORM 1300-RETURN-STATEFULLY.
+
+       1100-INITIALIZE.
+      *    CLEAR SESSION STATE AND MAP FIELDS
+           INITIALIZE WS-SESSION-STATE.
+           INITIALIZE ESONMO.
+
+      *    FOR THE FIRST INTERACTION, WE SEND THE MAP WITH JUST
+      *    THE TRANSACTION ID ON IT
+           MOVE EIBTRNID TO TRANIDO.
+
+       1200-SEND-MAP.
+      *    SENDS MAP TO THE USER
+           EXEC CICS SEND
+                MAP('ESONM')
+                MAPSET('ESONMAP')
+                FROM (ESONMO)
+                ERASE
+                END-EXEC.
+
+       1300-RETURN-STATEFULLY.
+      *    RETURNS SAVING THE CURRENT SESSION STATE
+      *    AND THE CONVERSATION WILL KEEP GOING
+           EXEC CICS RETURN
+                COMMAREA(WS-SESSION-STATE)
+                TRANSID(EIBTRNID)
+                END-EXEC.
+
+       2000-PROCESS-USER-INPUT.
+      *    THIS IS THE CONTINUATION OF THE CONVERSATION,
+      *    MEANING THE SECOND INTERACTION OF THE PROCESS,
+      *    HENCE THE COMM-AREA IS NOT EMPTY
+           PERFORM 2100-RESTORE-STATE.
+           PERFORM 2200-RECEIVE-MAP.
+           PERFORM 2300-UPDATE-STATE.
+           PERFORM 2400-MAKE-GREETING.
+
+      *    SEND THE MAP BACK WITH A GREETING
+           PERFORM 1200-SEND-MAP.
+           PERFORM 2500-RETURN-AND-END.
+
+       2100-RESTORE-STATE.
+      *    RESTORE PREVIOUS SESSION DATA INTO MY LOCAL VARS
+           MOVE DFHCOMMAREA TO WS-SESSION-STATE.
+
+       2200-RECEIVE-MAP.
+      *    GET INPUT FROM THE USER
+           EXEC CICS RECEIVE
+                MAP('ESONM')
+                MAPSET('ESONMAP')
+                INTO (ESONMI)
+                END-EXEC.
+
+       2300-UPDATE-STATE.
+      *    IF NEW DATA WAS RECEIVED, UPDATE STATE
+           IF USERIDI IS NOT EQUAL TO LOW-VALUES
+              MOVE USERIDI TO WS-USER-ID
+           END-IF.
+           IF PASSWDI IS NOT EQUAL TO LOW-VALUES
+              MOVE PASSWDI TO WS-USER-PASSWORD
+           END-IF.
+
+       2400-MAKE-GREETING.
+      *    GREET THE USER WITH A MESSAGE
+           INITIALIZE MESSO.
+
+           STRING "Hello " DELIMITED BY SIZE
+                  WS-USER-ID DELIMITED BY SPACE
+                  "!" DELIMITED BY SIZE
+              INTO MESSO
+           END-STRING.
+           
+       2500-RETURN-AND-END.
+      *    THIS ENDS THE CICS CONVERSATION
+           EXEC CICS RETURN
+                END-EXEC.
+                
